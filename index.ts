728eb087d3bc93d98b038e579a2e99891af55f18
Lorenzo Corbella <lorenzo.corbella@accenture.com>
2025-09-09 16:56:21 +0200

    content and usage

--- index.ts
+++ index.ts
@@ -10,16 +10,19 @@ import terminal from "markdown-it-terminal";
 const md = new MarkdownIt();
 md.use(terminal);
 
-const { text, sources, providerMetadata} = await generateText({
+const { text, sources, providerMetadata, content,  usage, steps} = await generateText({
   model: perplexity('sonar-pro'),
   prompt: 'What are the latest developments in quantum computing? Reply in markdon format',
   providerOptions: {
     perplexity: {
       return_images: true, // Enable image responses (Tier-2 Perplexity users only)
+      /* seach_filter:'academic' DOES NOT WORK!!! */
     },
   },
 });
 
 console.log('Result: ', md.render(text));
-// console.log('Sources: ', sources);
+console.log('Sources: ', sources);
 // console.log('providerMetadata: ', providerMetadata);
+console.log("Usage: ", usage); // statistiche
+console.log('Content: ', content) // array di obj con type text e type sources
